6240f1262b191fedcd4e57b6e700719074e2e679
Raphael Pinel <36964192+raphaelpinel@users.noreply.github.com>
2023-09-21 19:32:53 +0300

    Add blocking request

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+function delay(durationInMs) {
+  const startTime = new Date();
+  while (new Date() - startTime < durationInMs) {
+    // do nothing
+    // event loop is blocked...
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Node.js performance example");
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send("timer finished");
 });
 
 app.listen(port, () => {
